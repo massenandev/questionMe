98028a87b1c9ff57e3fbd298233f11feac714366
massenandev <massenandev@gmail.com>
2021-06-13 12:22:14 -0300

    building question form

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function(req,res){
-  res.render("index")
+  res.render('index')
+})
+
+app.get('/ask', (req, res) => {
+  res.render('ask')
 })
 
 app.listen(8081, () => { console.log("app running") })
