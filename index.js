8730de47712eb04e3e07861a4f8163e4814cebe1
massenandev <massenandev@gmail.com>
2021-06-12 22:45:01 -0300

    conditions statements using ejs

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express()
 //usar o ejs como view engine
 app.set('view engine', 'ejs')
 
-app.get('/', function(req,res){
+app.get('/:name/:lang', function(req,res){
   //render procura diretamente na pasta views
-  res.render("index")
+  let name = req.params.name
+  let lang = req.params.lang
+  let showMsg = false
+  res.render("index", {
+    name,
+    lang,
+    company: "Co",
+    subscribers: 8000,
+    msg: showMsg
+  })
 })
 
 app.listen(8081, () => { console.log("app running") })
